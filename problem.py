d7c2b9f529822079f4555353e7d8f922720a9e7d
Kelemen Péter <peter.kelemen456@gmail.com>
2024-02-22 11:12:48 +0100

    Added Problem class

--- problem.py
+++ problem.py
@@ -5,3 +5,22 @@ class Problem:
 
     def actions(self, state):
         raise NotImplementedError
+
+    def result(self, state, action):
+        raise NotImplementedError
+
+    def goal_test(self, state):
+        if isinstance(self.goal, list):
+            for s in self.goal:
+                if s == state:
+                    return True
+
+            return False
+        else:
+            return state == self.goal
+
+    def path_cost(self, c, state1, action, state2):
+        return c + 1
+
+    def value(self, state):
+        raise NotImplementedError
